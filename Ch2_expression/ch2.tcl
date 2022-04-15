755f21c5328afdfbc6e351e9e168618a02c0e183
DESKTOP-KG2UIRC\72721 <727216411@qq.com>
2022-04-15 19:49:41 +0800

    表达式 操作符

---
+++ Ch2_expression/ch2.tcl
@@ -0,0 +1,17 @@
+set pi 3.14
+set a 2
+
+puts [expr $pi*$a]
+#6.28
+
+puts [expr {1/2}]
+#0
+
+puts [expr abs(-3.2)]
+#3.2
+
+puts [expr exp(1)]
+#2.718281828459045
+
+puts [expr ceil(3.6)]
+#4.0 向上取整
